ce6559f603c8156f185086e77694d9c5be79383b
Kenneth <obikennethe913@gmail.com>
2022-07-21 14:28:33 +0200

    set min and max for input type number

--- amazon-clone-fe/src/components/Input.jsx
+++ amazon-clone-fe/src/components/Input.jsx
@@ -8,6 +8,8 @@ const Input = (props) => {
         type={type}
         name={name}
         id={name}
+        min={1}
+        max={5}
         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
         placeholder=" "
         required
